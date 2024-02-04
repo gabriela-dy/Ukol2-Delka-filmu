74b693a57b50265564034731bdc8d36e7ac0daf6
Gabriela Dyworova <gabriela.dyworova@gmail.com>
2024-02-04 11:03:13 +0100

    mam err

---
+++ script.js
@@ -0,0 +1,10 @@
+document.innerHTML = "<h1>Výpočet délky filmu</h1>";
+
+
+document.innerHTML = '<br>';
+
+let hodiny = Math.floor(223 / 60);
+let minut = 223 % 60;
+
+document.innerHTML = "Rozšířená verze filmu: Pán prstenů: Dvě věže trvá " + hodiny + "hodin a" + minut + " minut.";
+
