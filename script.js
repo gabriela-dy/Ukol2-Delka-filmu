4a6f7b2d79912c16b1548275dfef5656bab7663d
Gabriela Dyworova <gabriela.dyworova@gmail.com>
2024-02-09 16:53:31 +0100

    uprava

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.innerHTML = "<h1>Výpočet délky filmu</h1>"
+document.body.innerHTML = "<h1>Výpočet délky filmu</h1>"
 
 let hodiny = Math.floor(223 / 60)
 let minut = 223 % 60
 
-document.innerHTML = "Rozšířená verze filmu: Pán prstenů: Dvě věže trvá " + hodiny + "hodin a" + minut + " minut."
+document.body.innerHTML = "Rozšířená verze filmu: Pán prstenů: Dvě věže trvá " + hodiny + " hodin a " + minut + " minut."
